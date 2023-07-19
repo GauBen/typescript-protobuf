0a688ea0a38bd860d06a42a9f6aba2b3721444c2
Gautier <gautier@escape.tech>
2023-07-19 17:10:16 +0200

    feat: protobuf event bus

--- src/bus.ts
+++ src/bus.ts
@@ -1,14 +1,32 @@
+import type { PartialMessage } from "@bufbuild/protobuf";
 import EventEmitter from "node:events";
+import { Join, Leave, Message } from "./proto/chat_pb.js";
 
 const bus = new EventEmitter();
 
-export const emit = (type: string, detail?: any) => {
-  bus.emit(type, detail);
+const messages = {
+  message: Message,
+  join: Join,
+  leave: Leave,
 };
 
-export const listen = (type: string, callback: (detail?: any) => void) => {
-  bus.addListener(type, callback);
+export const emit = <T extends keyof typeof messages>(
+  type: T,
+  // @ts-expect-error There are type errors on `equal` methods
+  detail: PartialMessage<InstanceType<(typeof messages)[T]>>
+) => {
+  bus.emit(type, new messages[type](detail).toBinary());
+};
+
+export const listen = <T extends keyof typeof messages>(
+  type: T,
+  callback: (message: InstanceType<(typeof messages)[T]>) => void
+) => {
+  const listener = (raw: Uint8Array) => {
+    callback(messages[type].fromBinary(raw) as any);
+  };
+  bus.addListener(type, listener);
   return () => {
-    bus.removeListener(type, callback);
+    bus.removeListener(type, listener);
   };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { fileURLToPath } from "node:url";
 import * as bus from "./bus.js";
+import { Leave, Message } from "./proto/chat_pb.js";
 
 const app = express();
 
@@ -16,26 +17,35 @@ app.get("/messages", (req, res) => {
     bus.listen("message", (message) => {
       res.write(`data: ${JSON.stringify(message)}\r\n\r\n`);
     }),
-    bus.listen("join", (username) => {
+    bus.listen("join", ({ username }) => {
       res.write(`event: join\r\ndata: ${username}\r\n\r\n`);
     }),
-    bus.listen("leave", (username) => {
+    bus.listen("leave", ({ username }) => {
       res.write(`event: leave\r\ndata: ${username}\r\n\r\n`);
     }),
   ];
 
   const username = String(req.query.username);
-  bus.emit("join", username);
+  bus.emit("join", { username });
 
   req.on("close", () => {
     for (const cleanup of cleanups) cleanup();
-    bus.emit("leave", username);
+    bus.emit("leave", { username });
   });
 });
 
 app.post("/send", express.json(), (req, res) => {
-  bus.emit("message", req.body);
-  res.sendStatus(204);
+  try {
+    // Use protobuf to validate the message
+    const message = Message.fromJson(req.body);
+
+    bus.emit("message", message);
+    console.log(message.username + ":", message.body);
+    res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) res.status(400).end(error.message);
+    else res.sendStatus(500);
+  }
 });
 
 app.get("*", express.static(fileURLToPath(new URL("public", import.meta.url))));
